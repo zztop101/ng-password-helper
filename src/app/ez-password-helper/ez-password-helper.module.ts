038c23b5df994b7881856b4e3e65de8931f03f40
Terry Roberts <terry.roberts@rms.nsw.gov.au>
2016-10-21 14:51:39 +1100

    fix error

--- src/app/ez-password-helper/ez-password-helper.module.ts
+++ src/app/ez-password-helper/ez-password-helper.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EzPasswordHelperComponent } from './ez-password-helper.component';
 import { EzPasswordValidatorDirective } from './ez-password-validator.directive';
-import { EzPasswordValidatorDirective } from './ez-password-helper/ez-password-validator.directive';
 
 @NgModule({
   imports: [
