9dde0a5443780c470f1b920dd11e9ee278c216ab
Terry <trob67@hotmail.com>
2016-10-23 20:42:46 +1100

    fix up to delete error if it exists

--- src/app/ez-password-helper/ez-password-validator.directive.ts
+++ src/app/ez-password-helper/ez-password-validator.directive.ts
@@ -20,9 +20,8 @@ export class EzPasswordValidatorDirective implements Validator {
         let value = control.value;
 
         if (value && this.ezPasswordRulesService.validPassword(value)) {
-          delete control.errors['ezInvalid'];
-          if (!Object.keys(control.errors).length) {
-             control.setErrors(null);
+          if (control.errors) {
+            delete control.errors['ezInvalid'];
           }
           valid = true;
         }
