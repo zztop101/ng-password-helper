56b525dd09c16485175f09a313523fbed9e261e7
Terry Roberts <terry.roberts@rms.nsw.gov.au>
2016-10-24 14:49:19 +1100

    make rules customisable

--- src/app/ez-password-helper/ez-password-rules.service.ts
+++ src/app/ez-password-helper/ez-password-rules.service.ts
@@ -6,16 +6,26 @@ import {Rule} from './rule';
 @Injectable()
 export class EzPasswordRulesService {
 
-  private rules: Rule[];
+  private rules: Rule[] = new Array();
   private rulesdata: BehaviorSubject<Rule[]> = new BehaviorSubject(this.rules);
 
+  public RULE_LENGTH: Rule = {desc: 'At least 8 and no more than 32 characters', regex: new RegExp('^.{8,32}$'), valid: false };
+  public RULE_UPPER: Rule = {desc: 'At least one upper case character', regex: new RegExp('.*[A-Z].*'), valid: false};
+  public RULE_LOWER: Rule = {desc: 'At least one lower case character', regex: new RegExp('.*[a-z].*'), valid: false};
+  public RULE_DIGIT: Rule = {desc: 'At least one digit', regex: new RegExp('.*[0-9].*'), valid: false};
+  public RULE_SPECIAL: Rule = {desc: 'At least one special character',  regex: new RegExp('.*[!@#$%^&+=].*'),  valid: false }
+
+
+  /**
+   * set up the default rules, can be overriden by the application
+   * calling setRules.
+   */
   constructor() {
-    this.rules = new Array();
-    this.rules.push({desc: 'At least 8 and no more than 32 characters', regex: new RegExp('^.{8,32}$'), valid: false });
-    this.rules.push({desc: 'At least one upper case character', regex: new RegExp('.*[A-Z].*'), valid: false});
-    this.rules.push({desc: 'At least one lower case character', regex: new RegExp('.*[a-z].*'), valid: false});
-    this.rules.push({desc: 'At least one digit', regex: new RegExp('.*[0-9].*'), valid: false});
-    this.rules.push({desc: 'At least one special character',  regex: new RegExp('.*[!@#$%^&+=].*'),  valid: false });
+    this.rules.push(this.RULE_LENGTH);
+    this.rules.push(this.RULE_UPPER);
+    this.rules.push(this.RULE_LOWER);
+    this.rules.push(this.RULE_DIGIT);
+    this.rules.push(this.RULE_SPECIAL);
   }
 
   public getRulesData(): Observable<any[]> {
@@ -26,7 +36,22 @@ export class EzPasswordRulesService {
     return this.rules;
   }
 
-  public validPassword(value) {
+  /**
+   * Override the default rules.
+   *
+   * @param  {Rule[]} rules
+   */
+  public setRules(rules: Rule[]) {
+    this.rules = rules;
+  }
+
+  /**
+   * Check to see if password entered passes the rules criteria.
+   *
+   * @param  {} value
+   * return true or false if all rules criteria has been met.
+   */
+  public validPassword(value: string) {
     let validCntr = 0;
     this.rules.forEach(
       function (element) {
